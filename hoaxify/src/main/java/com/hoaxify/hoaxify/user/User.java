251c95a0daccdf6065a9b868082ac22a7951dff0
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-11 13:44:24 +0000

    2023/02/11 (am)

---
+++ hoaxify/src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -0,0 +1,16 @@
+package com.hoaxify.hoaxify.user;
+
+import lombok.Data;
+
+@Data
+public
+class User {
+
+	private String userName;
+
+	private String displayName;
+
+	private String password;
+
+
+}
