98e7ab7ac5abebb0e08ca5c35154a5053429c4ee
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-12 09:08:53 +0000

    2023/02/12 (am)

--- hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -2,8 +2,11 @@ package com.hoaxify.hoaxify;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -13,26 +16,48 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.hoaxify.hoaxify.user.User;
+import user.User;
+import user.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class UserControllerTest {
 
+	private static final String API_1_0_USERS = "/api/1.0/users";
 	@Autowired
 	TestRestTemplate testRestTemplate;
 	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Before
+	public void cleanup() {
+		userRepository.deleteAll();
+	}
+	
 	@Test
 	public void postUser_whenUserIsValid_recieveOk() {
+		User user = createValidMethod();
+		ResponseEntity<Object> response = testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	
+	@Test
+	public void postUser_whenUserIsValid_userSavedToDatabase() {
+		User user = createValidMethod();
+		testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+		assertThat(userRepository.count()).isEqualTo(1);
+	}
+	
+	private User createValidMethod() {
 		User user = new User();
 		user.setUserName("test-user");
 		user.setDisplayName("test-display");
 		user.setPassword("P4ssword");
-		
-		ResponseEntity<Object> response = testRestTemplate.postForEntity("/api/1.0/users", user, Object.class);
-		
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		return user;
 	}
 }
